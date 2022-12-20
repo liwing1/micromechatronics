3e0e291ecd4ba4094724f4240c6e46b1c91dbaf3
Li Wing <liwramos1@gmail.com>
2022-12-20 13:09:22 +0900

    fix first time

--- ADS2021-2/ioData/src/inpData_imu.cpp
+++ ADS2021-2/ioData/src/inpData_imu.cpp
@@ -53,27 +53,33 @@ int inpData_imu::calcIMU( void )
 	static double theta_gyro[3] = {0};
 	double theta_accel[3] = {0};
 	double g_angvel[3] = {0};
+	static bool first_time = true;
 
-	theta_accel[_ROLL] = atan(accel[_Y]/accel[_Z]);
-	theta_accel[_PITCH] = atan(accel[_Y]/sqrt(pow(accel[_X], 2) + pow(accel[_Z], 2)));
+	if(first_time){
+		first_time = false;
+		old_time = time;
+	} else {
+		theta_accel[_ROLL] = atan(accel[_Y]/accel[_Z]);
+		theta_accel[_PITCH] = atan(accel[_Y]/sqrt(pow(accel[_X], 2) + pow(accel[_Z], 2)));
 
-	g_angvel[_ROLL] = 	angvel[_X] + 
-						angvel[_Y] * sin(theta_gyro[_ROLL]) * tan(theta_gyro[_PITCH]) +
-						angvel[_Z] * cos(theta_gyro[_ROLL]) * tan(theta_gyro[_PITCH]) ;
-	g_angvel[_PITCH] =	angvel[_Y] * cos(theta_gyro[_ROLL]) -
-						angvel[_Z] * sin(theta_gyro[_ROLL]);
-	g_angvel[_YAW] = 	angvel[_Y] * (sin(theta_gyro[_ROLL])/cos(theta_gyro[_PITCH])) +
-						angvel[_Z] * (cos(theta_gyro[_ROLL])/cos(theta_gyro[_PITCH]));
+		g_angvel[_ROLL] = 	angvel[_X] + 
+							angvel[_Y] * sin(theta_gyro[_ROLL]) * tan(theta_gyro[_PITCH]) +
+							angvel[_Z] * cos(theta_gyro[_ROLL]) * tan(theta_gyro[_PITCH]) ;
+		g_angvel[_PITCH] =	angvel[_Y] * cos(theta_gyro[_ROLL]) -
+							angvel[_Z] * sin(theta_gyro[_ROLL]);
+		g_angvel[_YAW] = 	angvel[_Y] * (sin(theta_gyro[_ROLL])/cos(theta_gyro[_PITCH])) +
+							angvel[_Z] * (cos(theta_gyro[_ROLL])/cos(theta_gyro[_PITCH]));
 
-	double dt = time - old_time;
-	old_time = time;
+		double dt = time - old_time;
+		old_time = time;
 
-	for(uint8_t i = 0; i < 3; i++){
-		theta_gyro[i] += g_angvel[i] * dt;
-	}
+		for(uint8_t i = 0; i < 3; i++){
+			theta_gyro[i] += g_angvel[i] * dt;
+		}
 
-	for(uint8_t i = 0; i < 3; i++){
-		theta[i] = theta_accel[i] + K*(theta_gyro[i] - theta_accel[i]);
+		for(uint8_t i = 0; i < 3; i++){
+			theta[i] = theta_accel[i] + K*(theta_gyro[i] - theta_accel[i]);
+		}
 	}
 
 	return EXIT_SUCCESS;
